51274cce0db98a9ae23675596708b20b5a8e9b37
Edu58 <edumuriithi58@gmail.com>
2023-03-18 22:01:15 +0300

    feat: add notify_loanee to loan model

--- main/admin.py
+++ main/admin.py
@@ -1,25 +1,64 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import *
 
 
 class CustomUserAdmin(UserAdmin):
-    search_fields = ('email', 'first_name', 'last_name', )
-    ordering = ('-created_at', )
-    list_filter = ('email', 'first_name', 'last_name',  'is_active', 'is_staff', )
-    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', )
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+    ordering = ("-created_at",)
+    list_filter = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+    )
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+    )
 
     fieldsets = (
-        ('Personal', {'fields': ('email', 'first_name', 'last_name', 'password', )}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (
+            "Personal",
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
     )
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide', ),
-            'fields': ('email', 'first_name', 'last_name','password1', 'password2', 'is_active', 'is_staff', 'is_superuser', ),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
     )
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register([Loan])
